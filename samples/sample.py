e1903a658772977414fb42c01a4a2a6a80643b18
karampitsos <nikolaos.karampitsos@gmail.com>
2022-06-17 17:04:20 +0300

    naming of files

--- samples/sample.py
+++ samples/sample.py
@@ -2,10 +2,10 @@ from phos import AvantesSpectra
 import matplotlib.pyplot as plt
 
 
-spectra = AvantesSpectra('../input/1904175U1_0001.raw8')
+spectra = AvantesSpectra('../input/example.raw8')
 print(f'integration time: {spectra.integration_time}')
 print(f'integration delay: {spectra.integration_delay}')
 plt.plot(spectra.wavelenghts, spectra.counts)
 plt.show()
-spectra.to_csv('./output/test.csv')
-spectra.to_json('./output/file.json')
+spectra.to_csv('file.csv')
+spectra.to_json('file.json')
